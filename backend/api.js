b881fdad143b245c4542095a7861a98da1f0a8ec
Kai Hermann <kai.uwe.hermann@gmail.com>
2012-06-24 03:28:03 +0200

    added the removed field to the data that is returned by the getshoppinglist and getshoppinglistupdates api calls + added information about this change to the documentation
    Signed-off-by: Kai Hermann <kai.uwe.hermann@gmail.com>

--- backend/api.js
+++ backend/api.js
@@ -27,17 +27,18 @@ exports.initAPI = function(app,connection) {
 			if(rows[0] != null) {
 				if(rows[0].wg_id == req.user.wg) {
 					connection.query('SELECT * FROM items WHERE shopping_list_id = '+connection.escape(req.params.id), function(err, itemrows, fields) {
-						function shoppinglistitem(description,insert_date,checked,user_id,purchase_id)
+						function shoppinglistitem(description,insert_date,checked,removed,user_id,purchase_id)
 						{
 							this.description=description;
 							this.insert_date=insert_date;
 							this.checked=checked;
+							this.removed=removed;
 							this.user_id=user_id;
 							this.purchase_id=purchase_id;
 						}
 						var items = [];
 						for(var item in itemrows) {
-							items[item] = new shoppinglistitem(itemrows[item].description,itemrows[item].insert_date,itemrows[item].checked,itemrows[item].user_id,itemrows[item].purchase_id);
+							items[item] = new shoppinglistitem(itemrows[item].description,itemrows[item].insert_date,itemrows[item].checked,itemrows[item].removed,itemrows[item].user_id,itemrows[item].purchase_id);
 						}
 						//update user update time
 						req.user.date = new Date();
@@ -56,17 +57,18 @@ exports.initAPI = function(app,connection) {
 			if(rows[0] != null) {
 				if(rows[0].wg_id == req.user.wg) {
 					connection.query('SELECT * FROM items WHERE shopping_list_id = '+connection.escape(req.params.id)+' AND update_date > '+connection.escape(req.user.date), function(err, itemrows, fields) {
-						function shoppinglistitem(description,insert_date,checked,user_id,purchase_id)
+						function shoppinglistitem(description,insert_date,checked,removed,user_id,purchase_id)
 						{
 							this.description=description;
 							this.insert_date=insert_date;
 							this.checked=checked;
+							this.removed=removed;
 							this.user_id=user_id;
 							this.purchase_id=purchase_id;
 						}
 						var items = [];
 						for(var item in itemrows) {
-							items[item] = new shoppinglistitem(itemrows[item].description,itemrows[item].insert_date,itemrows[item].checked,itemrows[item].user_id,itemrows[item].purchase_id);
+							items[item] = new shoppinglistitem(itemrows[item].description,itemrows[item].insert_date,itemrows[item].checked,itemrows[item].removed,itemrows[item].user_id,itemrows[item].purchase_id);
 						}
 						//update user update time
 						req.user.date = new Date();
